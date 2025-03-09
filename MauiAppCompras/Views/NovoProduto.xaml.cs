33ecb972aed79d26738fbc133e36bf4c19e4f4c4
iago02770 <iagodt54@gmail.com>
2025-03-09 17:58:32 -0300

    Adição da Funcionalidade de Inseração no SQLite

--- MauiAppCompras/Views/NovoProduto.xaml.cs
+++ MauiAppCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,32 @@
+using MauiAppCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppCompras.Views;
 
 public partial class NovoProduto : ContentPage
 {
-	public NovoProduto()
-	{
-		InitializeComponent();
-	}
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
+
+    public async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso", "Registro Inserido", "OK!");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
