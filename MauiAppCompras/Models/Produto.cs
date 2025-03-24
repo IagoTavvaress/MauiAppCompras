f1be44ebae557c4729bf3fc55e6727d6125e73b4
IagoTavvaress <iagodt54@gmail.com>
2025-03-24 10:01:01 -0300

    Validação da inserção de produto e correção dos produtos duplicados na tela inicial

--- MauiAppCompras/Models/Produto.cs
+++ MauiAppCompras/Models/Produto.cs
@@ -8,21 +8,49 @@ namespace MauiAppMinhasCompras.Models
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
         public string Descricao
         {
             get => _descricao;
             set
             {
-                if (value == null)
+                if (string.IsNullOrEmpty(value))
                 {
-                    throw new Exception("Por favor, preencha a descrição");
+                    throw new Exception("Por favor, volte e preencha a descrição");
                 }
 
                 _descricao = value;
             }
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
-        public double Total { get => Quantidade * Preco; }
+
+        private double _quantidade;
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha uma quantidade válida maior que 0");
+                }
+                _quantidade = value;
+            }
+        }
+
+        private double _preco;
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Por favor, preencha um preço válido maior que 0");
+                }
+                _preco = value;
+            }
+        }
+
+        public double Total => Quantidade * Preco;
     }
 }
